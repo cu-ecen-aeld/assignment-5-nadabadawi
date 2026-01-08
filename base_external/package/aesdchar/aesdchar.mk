877c566e6604f99109c0f0fed078165c4fc6f9d4
nada <nada.badawi@fab-minds.com>
2026-01-08 13:52:27 +0200

    Assignment 8 Submission

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,24 @@
+
+##############################################################
+#
+# AESD-CHAR
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESDCHAR_VERSION = '7079d39c3d93df636508aa32a5369e488d4e2410'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-nadabadawi.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load   $(TARGET_DIR)/usr/bin/aesdchar_load
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/aesdchar_unload
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
